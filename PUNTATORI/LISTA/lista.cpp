1169eb1fdbf7a594be3fb2d8638e0e254c6fb5f0
Andrea-Bagli <bagliandrea618@gmail.com>
2024-03-18 08:43:14 +0000

    commit

---
+++ PUNTATORI/LISTA/lista.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "lista_concatenata.h"
+
+using namespace std;
+
+void lista::push(int x){
+
+    p = new nodo;
+    p -> info = x;
+    p -> next = testa;
+    testa = p; 
+}
+
+void lista::pop()
+{
+    p = testa;
+    testa = testa -> next;
+    delete p;
+}
+
+int lista::top()
+{
+    return testa -> info;
+}
+
+
+void oista::stampa()
+{
+     do{
+        cout<<p->info<<endl;
+        p -> next;
+    }while(p->next!=NULL);
+}
+   

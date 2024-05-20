5a51928e30417f7afc4ab8da5f3e1612e94552af
Andrea-Bagli <bagliandrea618@gmail.com>
2024-05-20 17:37:34 +0000

    commit

---
+++ CODA_PILA/cod.h
@@ -0,0 +1,17 @@
+#ifndef COD_H
+#define COD_H
+
+class coda{
+
+    private:
+
+        int nElementi;
+        int size;
+        int fine;
+        int inizio;
+        int *vet;
+
+    public:
+        
+
+}
